da340f2281b4a00b9e75ec861c9880a57e4b255d
Quentin Maisonneuve <quentin.maisonneuve@open-groupe.com>
2021-12-07 18:59:06 +0100

    Adding discord notification

---
+++ src/main/java/controller/service/notification/DiscordNotificationService.java
@@ -0,0 +1,11 @@
+package controller.service.notification;
+
+import model.GPUInfo;
+
+public class DiscordNotificationService implements NotificationService {
+
+    @Override
+    public void sendNotification(GPUInfo gpuInfo) {
+        // TODO
+    }
+}
